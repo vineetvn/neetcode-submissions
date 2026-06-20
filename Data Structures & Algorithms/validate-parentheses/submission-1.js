0e28b6dbc711f7fd298245ad3af12d33daa039a5
vineetvn <30828994+vineetvn@users.noreply.github.com>
2026-06-20 16:12:36 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        let stack = [];
+
+        if (s.length < 2) return false
+
+        for (let c of s) {
+            if (c === "{" || c === "(" || c === "[") {
+                stack.push(c)
+            }
+            else {
+                if (!stack.length) return false
+
+                let top = stack.pop()
+
+                if (c === "}" && top !== "{") return false
+                if (c === ")" && top !== "(") return false
+                if (c === "]" && top !== "[") return false
+            }
+        }
+
+        return !stack.length
+    }
+}
